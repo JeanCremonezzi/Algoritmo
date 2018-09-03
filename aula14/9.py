e6817e8891207ee243d15bddeb0423f71e5a7b97
JeanCremonezzi <jeancremonezzi@hotmail.com>
2018-09-03 15:15:55 -0300

    Signed-off-by: JeanCremonezzi <jeancremonezzi@hotmail.com>

--- aula14/9.py
+++ aula14/9.py
@@ -3,17 +3,24 @@ p2 = input("Palavra Comparar: ").lower()
 
 a = ""
 
-for x in p1:
-    if p1.count(x) == p2.count(x):
+ep1 = ""
+ep2 = ""
+
+for y in p1:
+    ep1 = (ep1 + y).rstrip()
+
+for y in p2:
+    ep2 = (ep2 + y).rstrip()
+
+for x in ep1:
+    if ep1.count(x) == ep2.count(x):
         a = a + x
 
-if p1 == "" or p2 == "":
+if ep1 == "" or ep2 == "":
     print ('NÃO é anagrama')
 
-elif len(a) == len(p1):
+elif len(a) == len(ep1):
     print ('É anagrama')
 
 else:
     print ('NÃO é anagrama')
-
-
