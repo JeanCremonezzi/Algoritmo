d1a3b9bb5bd51de8b6b724fb8e488474ac7f7ef2
JeanCremonezzi <jeancremonezzi@hotmail.com>
2018-09-03 15:23:42 -0300

    Signed-off-by: JeanCremonezzi <jeancremonezzi@hotmail.com>

--- aula14/3.py
+++ aula14/3.py
@@ -5,10 +5,10 @@ lenp1 = len(p1)
 lenp2 = len(p2)
 
 if p1 == p2:
-    print ('p1 (',p1,') = p2 (',p2,')')
+    print ('p1 (',p1,') é igual p2 (',p2,')')
 
 else:
-    print ('p1 (',p1,') != p2 (',p2,')')
+    print ('p1 (',p1,') é diferente de p2 (',p2,')')
 
 print ('Quantidade de caracteres p1 = ',len(p1))
 print ('Quantidade de caracteres p2 = ',len(p2))
--- aula14/8.py
+++ aula14/8.py
@@ -1,41 +1,16 @@
 frase = input("Frase: ")
 vogais = "aeiouAEIOU"
 total_vogais= 0
-a = 0
-e = 0
-i = 0
-o = 0
-u = 0
 espaco = 0
 
 for x in frase:
     if x in vogais:
         total_vogais = total_vogais + 1
 
-    if x == "a" or x == "A":
-        a = a +1
-
-    if x == "e" or x == "E":
-        e = e +1
-
-    if x == "i" or x == "I":
-        i = i +1
-
-    if x == "o" or x == "O":
-        o = o +1
-        
-    if x == "u" or x == "U":
-        u = u +1
-
     if x == " ":
         espaco = espaco +1
     
 print ('Quantidade de Espaços:',espaco)
-print ('Quantidade de "A/a":',a)
-print ('Quantidade de "E/e":',e)
-print ('Quantidade de "I/i":',i)
-print ('Quantidade de "O/o":',o)
-print ('Quantidade de "U/u":',u)
 print ('Quantidade total de Vogais:',total_vogais)
         
     
