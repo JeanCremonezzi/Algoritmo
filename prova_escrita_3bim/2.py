2422f329ff2ba42f6faa9bf27f60ea5b1f1fe192
JeanCremonezzi <jeancremonezzi@hotmail.com>
2018-09-12 12:05:50 -0300

    Signed-off-by: JeanCremonezzi <jeancremonezzi@hotmail.com>

---
+++ prova_escrita_3bim/2.py
@@ -0,0 +1,21 @@
+l1 = [1,2,3,4,5]
+
+#a
+l1.insert(0,2)
+l1.insert(0,1)
+l1.insert(0,0)
+
+
+#b
+l1.append (6)
+
+
+#c
+qnt = 0
+for x in l1:
+    if x == 2:
+        qnt = qnt + 1
+print (qnt)
+
+
+#d
